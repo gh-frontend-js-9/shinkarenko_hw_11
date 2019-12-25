784039be8d11e2357714cb5df0266287d8ad9c48
Dimon Shinkarenko <dimonshinkarenko2001@gmail.com>
2019-12-25 21:12:04 +0200

    made some fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 // sign up form
-async function signUp() {
-  let signUpEmail = document.getElementById("signup-email").value;
-  let signUpPassword = document.getElementById("signup-password").value;
-  let signUpName = document.getElementById("signup-name").value;
-
-  let information = {
-    "email": signUpEmail,
-    "password": signUpPassword,
-    "name": signUpName
+async function signUp(email, password, name) {
+  const information = {
+    email: email,
+    password: password,
+    name: name
   };
   console.log(information);
 
@@ -22,15 +18,11 @@ async function signUp() {
   console.log(await response.json());
 }
 
-
 // login form
-async function login() {
-  let loginEmail = document.getElementById("login-email").value;
-  let loginPassword = document.getElementById("login-password").value;
-
-  let information = {
-    "email": loginEmail,
-    "password": loginPassword
+async function login(email, password) {
+  const information = {
+    email: email,
+    password: password
   };
   console.log(information);
 
@@ -45,17 +37,12 @@ async function login() {
   console.log(await response.json());
 }
 
-
 // reset password form
-async function reset() {
-  let resetEmail = document.getElementById("reset-email").value;
-  let resetPassword = document.getElementById("reset-password").value;
-  let confirmPassword = document.getElementById("confirm-password").value;
-
-  let information = {
-    "email": resetEmail,
-    "password": resetPassword,
-    "confirmationPassword": confirmPassword
+async function reset(email, password, confirmPassword) {
+  const information = {
+    email: email,
+    password:  password,
+    confirmationPassword: confirmPassword
   };
   console.log(information);
 
@@ -66,26 +53,98 @@ async function reset() {
     },
     body: JSON.stringify(information)
   };
-  let response = await fetch("http://localhost:3000/api/users/reset_password", options);
+  let response = await fetch("http://localhost:3000/api/users/reset_password/", options);
   console.log(await response.json());
 }
 
+//validation
+function validateEmail(emailForm, email) {
+  let emailformat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let emailErorr = document.getElementById("email-error");
+
+  if (!email.match(emailformat) && email.length > 0) {
+    emailForm.style.border = "2px solid red";
+    emailErorr.innerText = "You entered the incorrect mail";
+    emailErorr.style.display = "block";
+  } else if (email.length == 0) {
+    emailForm.style.border = "2px solid red";
+    emailErorr.innerText = "This is a mandatory form";
+    emailErorr.style.display = "block";
+  } else {
+    emailErorr.style.border = "1px solid black";
+    emailErorr.style.display = "none";
+  }
+}
+
+function validatePassword(passwordForm, password) {
+  let passwordErorr = document.getElementById("password-error");
+
+  if (password.length > 0 && password.length < 8) {
+    passwordForm.style.border = "2px solid red";
+    passwordErorr.innerText = "Your password must be longer than 8 characters";
+    passwordErorr.style.display = "block";
+  } else if (password.length == 0) {
+    passwordForm.style.border = "2px solid red";
+    passwordErorr.innerText = "This is a mandatory form";
+    passwordErorr.style.display = "block";
+  } else {
+    passwordForm.style.border = "1px solid black";
+    passwordErorr.style.display = "none";
+  }
+}
+
+function validateName(nameForm, name) {
+  let nameErorr = document.getElementById("name-erorr");
+
+  if (name.length > 0 && name.length < 3) {
+    nameForm.style.border = "2px solid red";
+    nameErorr.innerText = "Your name must be longer than 3 characters";
+    nameErorr.style.display = "block";
+  } else if (name.length == 0) {
+    nameForm.style.border = "2px solid red";
+    nameErorr.innerText = "This is a mandatory form";
+    nameErorr.style.display = "block";
+  } else {
+    nameForm.style.border = "1px solid black";
+    nameErorr.style.display = "none";
+  }
+}
 
 //buttons
 let signUpForm = document.getElementById("signup-form");
 signUpForm.addEventListener("submit", function(sub) {
+  const signUpEmail = document.getElementById("signup-email").value;
+  const signUpPassword = document.getElementById("signup-password").value;
+  const signUpName = document.getElementById("signup-name").value;
+  const signUpPasswordId = document.getElementById("signup-password");
+  const signUpEmailId = document.getElementById("signup-email");
+  const signUpNameId = document.getElementById("signup-name");
   sub.preventDefault();
-  signUp();
+  signUp(signUpEmail, signUpPassword, signUpName);
+  validateEmail(signUpEmailId, signUpEmail);
+  validatePassword(signUpPasswordId, signUpPassword);
+  validateName(signUpNameId, signUpName);
 });
+
 
 let loginForm = document.getElementById("login-form");
 loginForm.addEventListener("submit", function(sub) {
+  const loginEmail = document.getElementById("login-email").value;
+  const loginPassword = document.getElementById("login-password").value;
+  const loginEmailId = document.getElementById("login-email");
+  const loginPasswordId = document.getElementById("login-password");
   sub.preventDefault();
-  login();
+  login(loginEmail, loginPassword);
+  validateEmail(loginEmailId, loginEmail);
+  validatePassword(loginPasswordId, loginPassword);
 });
+
 
 let resetForm = document.getElementById("reset-form");
 resetForm.addEventListener("submit", function(sub) {
+  const resetEmail = document.getElementById("reset-email").value;
+  const resetPassword = document.getElementById("reset-password").value;
+  const confirmPassword = document.getElementById("confirm-password").value;
   sub.preventDefault();
-  reset();
+  reset(resetEmail,resetPassword,confirmPassword);
 });
